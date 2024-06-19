bc3d347b82fe2080294c30a8331b2d84924f2593
Flandrua <40861674+Flandrua@users.noreply.github.com>
2024-06-19 16:05:50 +0800

    Hamster导入lowpoly动物模型

--- Hamster/Project/Assets/FguiFramework/Core/Manager/Enum/SocketEvent.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SocketEvent
-{
-    public const string S2C_PING_PONG = "S2C_PingPong";
-    public const string C2S_PING_PONG = "C2S_PingPong";
-}
