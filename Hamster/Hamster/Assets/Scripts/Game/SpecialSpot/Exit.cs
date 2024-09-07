520f3edeab48daf5dc05a0ba7ba2225a41d16ba4
Flandrua <40861674+Flandrua@users.noreply.github.com>
2024-09-07 11:15:11 +0800

    Hamster： 增加了手机和结局触发

---
+++ Hamster/Hamster/Assets/Scripts/Game/SpecialSpot/Exit.cs
@@ -0,0 +1,33 @@
+using com;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class Exit : MonoBehaviour
+{
+    private Animator _animator;
+    private PlayerBehaviour _playerBehaviour;
+    private bool isPlayerExist = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if(DataCenter.Instance.GameData.Abilities.Contains(Ability.Phone))
+            {
+                //½á¾Ö
+            }
+        }
+    }
+}
--- Hamster/Hamster/Assets/Scripts/Game/SpecialSpot/Item.cs
+++ Hamster/Hamster/Assets/Scripts/Game/SpecialSpot/Item.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 public enum Ability
 {
     Spider,
-    Fish
+    Fish,
+    Phone
 }
 public class Item : MonoBehaviour
 {
